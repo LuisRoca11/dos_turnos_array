d923d306706cfbb8d76a71d2f6a0aeefe88377d8
Luis Roca <lroca1102@gmail.com>
2025-09-25 16:10:44 -0300

    version 1

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿// Una empresa tiene dos turnos (mañana y tarde) en los que trabajan 8 empleados (4 por la mañana y 4 por la tarde).
+// Confeccionar un programa que permita almacenar los sueldos de los empleados agrupados por turno.
+// Imprimir los gastos en sueldos de cada turno.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace dos_turnos_array
+{
+    class dos_turnos_array
+    {
+        private float[] turnoMan;
+        private float[] turnoTar;
+
+        float suma1 = 0;
+        float suma2 = 0;
+
+
+        public void Cargar()
+        {
+            turnoMan = new float[4];
+            turnoTar = new float[4];
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Ingrese el sueldo {i + 1} de la mañana: ");
+                turnoMan[i] = float.Parse(Console.ReadLine());
+                suma1 = suma1 + turnoMan[i];
+            }
+
+            for (int j = 0; j < 4; j++)
+            {
+                Console.WriteLine($"Ingrese el sueldo {j + 1} de la tarde: ");
+                turnoTar[j] = float.Parse(Console.ReadLine());
+                suma2 = suma2 + turnoTar[j];
+            }
+
+        }
+
+        public void Imprimir()
+        {
+            //suma1 = suma1 + turnoMan[i];
+            //suma2 = suma2 + turnoTar[j];
+            Console.WriteLine($"La suma de los sueldos de la mañana es: {suma1}");
+            Console.WriteLine($"La suma de los sueldos de la tarde es: {suma2}");
+        }
+
+        static void Main(string[] args)
+        {
+            dos_turnos_array pv = new float[];
+            pv.Cargar();
+            pv.Imprimir();
+        }
+        
+    }
+}
